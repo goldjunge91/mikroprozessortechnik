6cb4e42e0b649882ced96e77ba14a4fbe6df0a4f
goldjunge91 <m.tozzi91@icloud.com>
2025-06-11 02:02:34 +0200

    added gitignore and ignored debug files

--- Debug/src/subdir_rules.mk
+++ Debug/src/subdir_rules.mk
@@ -33,13 +33,6 @@ src/Aufgabe\ 3\ Alternative.obj: ../src/Aufgabe\ 3\ Alternative.c $(GEN_OPTS) | 
 	@echo 'Finished building: "$<"'
 	@echo ' '
 
-src/Aufgabe\ 3.obj: ../src/Aufgabe\ 3.c $(GEN_OPTS) | $(GEN_FILES) $(GEN_MISC_FILES)
-	@echo 'Building file: "$<"'
-	@echo 'Invoking: Arm Compiler'
-	"C:/ti/ccs1220/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/bin/armcl" -mv7M4 --code_state=16 --float_support=FPv4SPD16 -me --opt_for_speed=5 --include_path="C:/Users/tozzi/OneDrive - HAW-HH/Module/MP Mikroprozessortechnik/Labor/MPP1/Labor 4/labor4/ccs-project-0-win" --include_path="C:/ti/TivaWare_C_Series-2.2.0.295" --include_path="C:/ti/ccs1220/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include" --define=ccs="ccs" --define=PART_TM4C1294NCPDT -g --gcc --diag_warning=225 --diag_wrap=off --display_error_number --abi=eabi --preproc_with_compile --preproc_dependency="src/Aufgabe 3.d_raw" --obj_directory="src" $(GEN_OPTS__FLAG) "$<"
-	@echo 'Finished building: "$<"'
-	@echo ' '
-
 src/%.obj: ../src/%.c $(GEN_OPTS) | $(GEN_FILES) $(GEN_MISC_FILES)
 	@echo 'Building file: "$<"'
 	@echo 'Invoking: Arm Compiler'
--- Debug/src/subdir_vars.mk
+++ Debug/src/subdir_vars.mk
@@ -10,59 +10,53 @@ C_SRCS += \
 ../src/Aufgabe\ 1b.c \
 ../src/Aufgabe\ 1c.c \
 ../src/Aufgabe\ 3\ Alternative.c \
-../src/Aufgabe\ 3.c \
-../src/aufgabe_3_Final_v2.c \
-../src/labor_aufgabe_4.c \
-../src/run_aufgabe_4.c 
+../src/aufgabe_3.c \
+../src/aufgabe_4.c \
+../src/debugging.c 
 
 C_DEPS += \
 ./src/Aufgabe\ 1a.d \
 ./src/Aufgabe\ 1b.d \
 ./src/Aufgabe\ 1c.d \
 ./src/Aufgabe\ 3\ Alternative.d \
-./src/Aufgabe\ 3.d \
-./src/aufgabe_3_Final_v2.d \
-./src/labor_aufgabe_4.d \
-./src/run_aufgabe_4.d 
+./src/aufgabe_3.d \
+./src/aufgabe_4.d \
+./src/debugging.d 
 
 OBJS += \
 ./src/Aufgabe\ 1a.obj \
 ./src/Aufgabe\ 1b.obj \
 ./src/Aufgabe\ 1c.obj \
 ./src/Aufgabe\ 3\ Alternative.obj \
-./src/Aufgabe\ 3.obj \
-./src/aufgabe_3_Final_v2.obj \
-./src/labor_aufgabe_4.obj \
-./src/run_aufgabe_4.obj 
+./src/aufgabe_3.obj \
+./src/aufgabe_4.obj \
+./src/debugging.obj 
 
 OBJS__QUOTED += \
 "src\Aufgabe 1a.obj" \
 "src\Aufgabe 1b.obj" \
 "src\Aufgabe 1c.obj" \
 "src\Aufgabe 3 Alternative.obj" \
-"src\Aufgabe 3.obj" \
-"src\aufgabe_3_Final_v2.obj" \
-"src\labor_aufgabe_4.obj" \
-"src\run_aufgabe_4.obj" 
+"src\aufgabe_3.obj" \
+"src\aufgabe_4.obj" \
+"src\debugging.obj" 
 
 C_DEPS__QUOTED += \
 "src\Aufgabe 1a.d" \
 "src\Aufgabe 1b.d" \
 "src\Aufgabe 1c.d" \
 "src\Aufgabe 3 Alternative.d" \
-"src\Aufgabe 3.d" \
-"src\aufgabe_3_Final_v2.d" \
-"src\labor_aufgabe_4.d" \
-"src\run_aufgabe_4.d" 
+"src\aufgabe_3.d" \
+"src\aufgabe_4.d" \
+"src\debugging.d" 
 
 C_SRCS__QUOTED += \
 "../src/Aufgabe 1a.c" \
 "../src/Aufgabe 1b.c" \
 "../src/Aufgabe 1c.c" \
 "../src/Aufgabe 3 Alternative.c" \
-"../src/Aufgabe 3.c" \
-"../src/aufgabe_3_Final_v2.c" \
-"../src/labor_aufgabe_4.c" \
-"../src/run_aufgabe_4.c" 
+"../src/aufgabe_3.c" \
+"../src/aufgabe_4.c" \
+"../src/debugging.c" 
 
 
